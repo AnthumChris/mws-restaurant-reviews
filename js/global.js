5f7877b60378e926eda81cdb04f293c1a05e6985
Chris McGowan <chris@anthum.com>
2018-09-14 18:03:11 -0400

    feature: Set role=presentation on google maps images

--- js/global.js
+++ js/global.js
@@ -6,10 +6,14 @@ if ('serviceWorker' in navigator) {
   })
 }
 
-// add alt attribute to map <img> to improve Lighthouse accessibility score
+// to improve Lighthouse accessibility for google Maps images
+// https://www.w3.org/WAI/tutorials/images/decorative/
 function addAltToGoogleMapsImages() {
   // intentional delay required to capture UI control SVG images
   setTimeout(_ => {
-    this.getDiv().querySelectorAll('img').forEach(el => el.setAttribute('alt', 'Google Maps Tile Image'));
+    this.getDiv().querySelectorAll('img').forEach(el => {
+      // el.setAttribute('alt', '');
+      // el.setAttribute('role', 'presentation');
+    });
   }, 250);
 }
